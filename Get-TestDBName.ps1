9940d800bf7616ed9969cc85e0cdc634e418fa11
raunit-ranjan <51176670+raunit-ranjan@users.noreply.github.com>
2019-05-30 01:56:35 +0530

    Add files via upload

---
+++ Get-TestDBName.ps1
@@ -0,0 +1,46 @@
+﻿function Get-TestDBName{
+param( [string] $fileName, [string] $targetPath)
+
+$tempPath = '.\tempUnzipped'
+
+if (test-path($tempPath)){
+        Remove-Item $tempPath -Recurse -Force
+    }
+
+
+if (-not (test-path($targetPath))){
+        New-Item $targetPath -ItemType Directory
+    }
+
+expand-archive -path $fileName -destinationpath '.\tempUnzipped' 
+
+Get-ChildItem -Path $tempPath -Filter '*.zip' | ForEach-Object{ 
+    $fileName = $_.Name.Replace(".zip","")
+    expand-archive -Path $tempPath\$_ -destinationpath "$tempPath\$fileName"
+    
+    $files = $(Get-Childitem -Path "$tempPath\$fileName")
+     
+    foreach($file in $files) {
+        
+        copy-item $file.fullname -destination  "$targetPath" -Force
+
+        $newFilename = $fileName.Split(".")[0] + $file.extension
+
+        if (test-path("$targetPath\$newFilename")){
+        Remove-Item  "$targetPath\$file"    
+        }
+        else
+        {
+        rename-item         "$targetPath\$file"    "$newFilename"          
+        }
+        }
+
+    }
+
+
+    if (test-path($tempPath)){
+    Remove-Item $tempPath -Recurse -Force
+    }
+
+
+}
---
+++ Split-Files.ps1
@@ -0,0 +1,27 @@
+﻿function Split-Files{
+param( [string] $fileName, [string] $targetPath)
+
+$tempPath = '.\tempUnzipped'
+
+if (test-path($tempPath)){
+    Remove-Item $tempPath -Recurse -Force
+}
+
+if (-not (test-path($targetPath))){
+    New-Item $targetPath -ItemType Directory
+}
+
+expand-archive -path $fileName -destinationpath '.\tempUnzipped' 
+
+Get-ChildItem -Path $tempPath -Filter '*.zip' | expand-archive -destinationpath $tempPath 
+
+Get-ChildItem -Path $tempPath -Filter 'Enable*.txt' | compress-archive -destination "$targetPath\Enable.zip" -Force
+
+Get-ChildItem -Path $tempPath -Filter 'Disable*.txt' | compress-archive -destination "$targetPath\Disable.zip" -Force
+
+if (test-path($tempPath)){
+    Remove-Item $tempPath -Recurse -Force
+}
+
+
+}
